5ffe7ce9a90d858cfbb74c9e63695c86112328b1
Izaiah Rodriguez <zrodr@protonmail.com>
2022-04-10 17:42:08 -0700

    Added network components to IR_Scene and SingleTargetTower prefab

--- FinalProject/Assets/Scripts/TowerSpawners/Towers/SingleTarget.cs
+++ FinalProject/Assets/Scripts/TowerSpawners/Towers/SingleTarget.cs
@@ -8,16 +8,46 @@ public class SingleTarget : TowerTargeting
     [SerializeField] private float _stCooldown = 0.5f;
     [SerializeField] private int _stDamage = 25;
 
+    private GameObject _turretBarrel;
+
     void Start()
     {
         _targetingRadius = _stRadius;
         _cooldownTime = _stCooldown;
+
+        // as of now the barrel will be the first (and only) child of the root game object
+        Transform turretBarrelTransform = transform.GetChild(0);
+        _turretBarrel = turretBarrelTransform.gameObject;
     }
 
     void Update()
     {
 
         SearchForTargets();
+    }
+
+    // Handles the rotation of the tower to aim at the targeted enemy
+    private void SnapToEnemy(Transform enemyLocation)
+    {
+        float barrelTurnRateDegrees = 180f;
+
+        // Main turret body rotation: global y-axis to turn towards enemy
+        Vector3 turretBodyDirection = new Vector3(
+            enemyLocation.position.x,
+            transform.position.y, // locks y-axis rotation of main turret body
+            enemyLocation.position.z
+        );
+        transform.LookAt(turretBodyDirection);
+
+        // Turret barrel rotation: local x-axis to point directly at enemy
+        Vector3 barrelAimDirection = enemyLocation.position - _turretBarrel.transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(barrelAimDirection);
+
+        _turretBarrel.transform.rotation = Quaternion.RotateTowards(
+            _turretBarrel.transform.rotation,
+            targetRotation,
+            barrelTurnRateDegrees
+        );
     }
 
     // Single-Target Tower behavior: Find closest enemy and apply damage to them
@@ -45,13 +75,13 @@ public class SingleTarget : TowerTargeting
         // apply damage if enemy exists 
         if (nearest)
         {
-            Enemy enemy = nearest.GetComponent<Enemy>();
+            SnapToEnemy(nearest.transform);
+            //emy enemy = nearest.GetComponent<Enemy>();
 
-            enemy.Damage(this.Owner, this._stDamage);
-            
-            Debug.Log(string.Format("Applying {0} damage to [{1}] Total Health: {2}",
-                _stDamage, nearest.name, enemy.Health.HealthValue)
-            );
+           //eney.Damage(this.Owner, this._stDamage);
+            //  Debug.Log(string.Format("Applying {0} damage to [{1}] Total Health: {2}",
+            //    _stDamage, nearest.name, enemy.Health.HealthValue)
+            //);
         }
     }
 }
